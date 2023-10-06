32d078448a8b06e689717ec13f4f2fc1aa37702f
Ryan Faircloth <ryan@dss-i.com>
2023-10-06 15:25:40 -0400

    fix: use repo to avoid rate limit issues

--- .kind/ci.sh
+++ .kind/ci.sh
@@ -1,3 +1,5 @@
-helm upgrade --install --create-namespace --namespace strimzi strimzi oci://quay.io/strimzi-helm/strimzi-kafka-operator -f .kind/strimzi-operator-values.yaml --version ${V_STRIMZI_OPERATOR:-0.37.0} --wait
+# helm upgrade --install --create-namespace --namespace strimzi strimzi oci://quay.io/strimzi-helm/strimzi-kafka-operator -f .kind/strimzi-operator-values.yaml --version ${V_STRIMZI_OPERATOR:-0.37.0} --wait
+helm repo add strimzi https://strimzi.io/charts/
+helm upgrade --install --create-namespace --namespace strimzi strimzi/strimzi -f .kind/strimzi-operator-values.yaml --version ${V_STRIMZI_OPERATOR:-0.37.0} --wait
 helm repo add cert-manager https://charts.jetstack.io
 helm upgrade --install --create-namespace --namespace cert-manager cert-manager cert-manager/cert-manager --version ${V_CERT_MANAGER:-1.13.1} -f .kind/cert-manager-values.yaml --wait
