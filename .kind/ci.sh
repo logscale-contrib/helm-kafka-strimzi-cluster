20f06fe681695ea4e25294ea1cbf98cc05f91a1a
Ryan Faircloth <ryan@dss-i.com>
2024-09-19 12:09:25 -0400

    fix: update test version for lint

--- .kind/ci.sh
+++ .kind/ci.sh
@@ -2,7 +2,7 @@
 helm repo add strimzi https://strimzi.io/charts/
 helm repo add cert-manager https://charts.jetstack.io
 helm upgrade --install --create-namespace --namespace strimzi \
-    strimzi strimzi/strimzi-kafka-operator --version ${V_STRIMZI_OPERATOR:-0.37.0} \
+    strimzi strimzi/strimzi-kafka-operator --version ${V_STRIMZI_OPERATOR:-0.43.0} \
     -f .kind/strimzi-operator-values.yaml --wait
 helm upgrade --install --create-namespace --namespace cert-manager \
     cert-manager cert-manager/cert-manager --version ${V_CERT_MANAGER:-1.13.1} \
